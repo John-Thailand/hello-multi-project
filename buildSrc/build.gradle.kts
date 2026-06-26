36717ac1ff08b44937d1bd0cf708e52e76ba12c7
John-Thailand <naoki.honda@mirrorfit.jp>
2026-06-26 17:07:10 +0900

    コメント追記

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,10 @@
+// buildSrcプロジェクトの設定ファイル
 plugins {
+  // Convention PluginをKotlin DSLで書けるようにするPlugin
   `kotlin-dsl`
 }
 
+// kotlin-dslを使うためにmavenCentralリポジトリからライブラリを取得するようにしたい
 repositories {
   mavenCentral()
 }
--- buildSrc/src/main/kotlin/java-common.gradle.kts
+++ buildSrc/src/main/kotlin/java-common.gradle.kts
@@ -1,3 +1,32 @@
+// java-common.gradle.kts
+// 実際に各サブプロジェクトへ適用するPlugin
+// この内容がappとserviceに適用される
+// 使う側で id("java-common")として設定する
+// Gradle開始
+//       │
+//       ▼
+// ① buildSrcを見つける
+//       │
+//       ▼
+// ② buildSrc/build.gradle.kts を実行
+//       │
+//       ▼
+// Kotlin DSL Pluginを使えるようにする
+//       │
+//       ▼
+// ③ java-common.gradle.kts をコンパイル
+//       │
+//       ▼
+// Convention Plugin完成
+//       │
+//       ▼
+// ④ app/build.gradle.kts
+// plugins {
+//     id("java-common")
+// }
+//       │
+//       ▼
+// java-commonの内容を適用
 plugins {
   java
 }
